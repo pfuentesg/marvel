8aefceb1da51433a28c2fab5cc357215e75fec2d
pfuentesg <pablofuentesg1@yahoo.es>
2017-10-22 14:36:28 +0200

    first commit

---
+++ tryal.js
@@ -0,0 +1,33 @@
+var PRIV_KEY = "4327ebc79cafc43d52c21ceba4830393";
+var PUBLIC_KEY = "72f14f6fc419aed657bc62223cf69c9e5e078ae9";
+
+function getMarvelResponse() {
+
+  // you need a new ts every request                                                                                    
+  var ts = new Date().getTime();
+  var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+  
+  // the api deals a lot in ids rather than just the strings you want to use
+  var characterId = '1009718'; // wolverine                                                                             
+
+
+  var url = 'http://gateway.marvel.com:80/v1/public/comics';
+
+  console.log(url);
+  $.getJSON(url, {
+    ts: ts,
+    apikey: PUBLIC_KEY,
+    hash: hash,
+    characters: characterId
+    })
+    .done(function(data) {
+      // sort of a long dump you will need to sort through
+      console.log(data);
+    })
+    .fail(function(err){
+      // the error codes are listed on the dev site
+      console.log(err);
+    });
+};
+
+getMarvelResponse();
